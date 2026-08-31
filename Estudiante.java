f3580444aa02b411d888eea73860d23de5cd7b62
Andy8701 <compadre8701@gmail.com>
2026-08-31 17:46:05 -0300

    TP0

---
+++ Estudiante.java
@@ -0,0 +1,20 @@
+public class Estudiante {
+    String nombre;
+    String apellido;
+    int edad;
+    String carrera;
+    double promedio;
+ 
+    public Estudiante() {
+    
+    }
+
+    public Estudiante(String nombre, String apellido, int edad, String carrera, double promedio) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.carrera = carrera;
+        this.promedio = promedio;
+
+    }
+}
